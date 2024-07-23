227e7239d329c597894c98d52d9969a87625841d
PhilipeX <dev.philipe@outlook.com>
2024-07-22 21:35:19 -0300

    feat(Mobile): Creates navigation for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,28 @@ import { useState} from "react";
 
 function App() {
   const [isHovered, setHovered] = useState(false);
+  const [isActive, setActive] = useState(false);
 
   return (
     <>
+      <nav className={`mobile-nav ${isActive ? "mobile-nav--visible" : "" }` }>
+        <div className="mobile-nav__close-btn"
+             onClick={()=> {setActive(false)}}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21">
+            <g fill="#D0D6F9" fillRule="evenodd">
+              <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z"/>
+              <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z"/>
+            </g>
+          </svg>
+        </div>
+        <div className="mobile-nav__links">
+          <a href="#"><span>00</span>HOME</a>
+          <a href="#"><span>01</span>DESTINATION</a>
+          <a href="#"><span>02</span>CREW</a>
+          <a href="#"><span>03</span>TECHNOLOGY</a>
+        </div>
+      </nav>
       <header className="header">
         <div className="header__logo">
           <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
@@ -16,7 +35,9 @@ function App() {
             </g>
           </svg>
         </div>
-        <div className="header__nav-icon">
+        <div className="header__nav-icon"
+        onClick={()=> {setActive(true)}}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
             <g fill="#D0D6F9" fillRule="evenodd">
               <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z"/>
